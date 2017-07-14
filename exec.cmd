3c72940c8271f80b796834409555b4f969cd74f3
le4ves <2463581400@qq.com>
2017-07-14 17:31:10 +0800

    Update exec.cmd

--- exec.cmd
+++ exec.cmd
@@ -1,1 +1,1 @@
-vcvarsall.bat & msbuild test0x03.sln /t:rebuild /p:configuration=release & cd x64\release & test0x03
+chcp 65001 & vcvarsall.bat & msbuild test0x03.sln /t:rebuild /p:configuration=release & cd x64\release & test0x03
